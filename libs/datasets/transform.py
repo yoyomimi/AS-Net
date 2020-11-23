146e64184643dd0bed605ef11824c453d2b8c208
chenmingfei <chenmingfei@sensetime.com>
2020-11-23 10:53:37 +0800

    update transform

--- libs/datasets/transform.py
+++ libs/datasets/transform.py
@@ -290,6 +290,7 @@ class Compose(object):
         format_string += "\n)"
         return format_string
 
+
 class EvalTransform(object):
     def __init__(self, mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225], 
                  max_size=1333):
